881cb2b5d4958818a368b07c13aad0918b6a8f34
antth <antth@hotmail.fi>
2019-06-28 16:23:26 +0300

    threshold

--- src/img_tools.py
+++ src/img_tools.py
@@ -1,4 +1,7 @@
+import os
+
 from PIL import Image
+import cv2
 
 from rectangle import Rectangle
 
@@ -10,4 +13,9 @@ def view_rectangle(rect: Rectangle, img_file: str):
 
 
 if __name__ == '__main__':
-    pass
+    os.chdir('../data')
+    image = cv2.imread('test.jpg')
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    thresh = cv2.threshold(gray, 64, 255, cv2.THRESH_BINARY)[1]
+    cv2.imshow('test', thresh)
+    cv2.waitKey(0)
