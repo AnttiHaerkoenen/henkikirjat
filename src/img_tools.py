9aaaacee613211c049fcaafcc7ec88c242f9225d
AnttiHaerkoenen <antth@hotmail.fi>
2019-06-29 21:46:50 +0300

    tweak

--- src/img_tools.py
+++ src/img_tools.py
@@ -2,6 +2,8 @@ import os
 
 from PIL import Image
 import cv2
+import numpy as np
+from skimage import morphology
 
 from rectangle import Rectangle
 
@@ -17,5 +19,5 @@ if __name__ == '__main__':
     image = cv2.imread('test.jpg')
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     thresh = cv2.threshold(gray, 64, 255, cv2.THRESH_BINARY)[1]
-    cv2.imshow('test', thresh)
-    cv2.waitKey(0)
+    contours, hierarchy = cv2.findContours(thresh, 1, 2)
+    cv2.imwrite('test.jpg', contours)
