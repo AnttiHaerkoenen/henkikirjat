222d3fce48cdafab3f4ac6add68584153c9b650d
antth <antth@hotmail.fi>
2019-09-09 17:03:47 +0300

    remove small regions

--- src/tools/bbox.py
+++ src/tools/bbox.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 from skimage.io import imread
 from skimage.util import invert
-from skimage.filters import threshold_otsu, threshold_sauvola
+from skimage.filters import threshold_yen
 from skimage.segmentation import clear_border
 from skimage.measure import label, regionprops
 from skimage.morphology import closing, square
@@ -17,25 +17,24 @@ if __name__ == '__main__':
     h, w, _ = image.shape
     image = invert(rgb2gray(image))[0:h // 2, 0:w // 2]
 
-    thresh = threshold_otsu(image)
+    thresh = threshold_yen(image)
     bw = image > thresh
-    bw = remove_lines(bw, threshold=250)
+    bw = remove_lines(bw, threshold=400)
 
     cleared = clear_border(bw)
 
-    # label image regions
+    label_image = label(bw)
+
+    for region in regionprops(label_image):
+        if region.area <= 100:
+            for (r, c) in region.coords:
+                bw[r, c] = 0
+
     label_image = label(bw)
     image_label_overlay = label2rgb(label_image, image=bw)
 
     fig, ax = plt.subplots(figsize=(10, 6))
     ax.imshow(image_label_overlay)
-
-    # for region in regionprops(label_image):
-    #     if 100 <= region.area <= 500:
-    #         minr, minc, maxr, maxc = region.bbox
-    #         rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
-    #                                   fill=False, edgecolor='red', linewidth=2)
-    #         ax.add_patch(rect)
 
     ax.set_axis_off()
     plt.tight_layout()
