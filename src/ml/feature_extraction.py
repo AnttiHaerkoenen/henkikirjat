161e76a7da6c4a4c24d47a19c11e8153fdf2f909
antth <antth@hotmail.fi>
2019-09-30 15:22:55 +0300

    downscaling

--- src/ml/feature_extraction.py
+++ src/ml/feature_extraction.py
@@ -7,19 +7,25 @@ from skimage.transform import downscale_local_mean
 
 
 def downscale_digits(
-        data,
+        data: pd.DataFrame,
         old_shape,
         new_shape,
         cval=0,
 ):
-    label, img = np.hsplit(data, [1])
-    factors = old_shape[0] / new_shape[0], old_shape[1] / new_shape[1]
-    # todo
-    return np.hstack([label, new])
+    label, img = data['label'], data.drop(columns=['label'])
+    factors = old_shape[0] // new_shape[0], old_shape[1] // new_shape[1]
+    new = []
+    for row in img.itertuples(index=False):
+        arr = np.array(row).reshape(old_shape)
+        arr = downscale_local_mean(arr, factors=factors, cval=cval)
+        new.append(arr.ravel())
+    new = pd.DataFrame(new)
+    concat = pd.concat([label, new], axis=1)
+    return concat
 
 
 if __name__ == '__main__':
     os.chdir('../../data')
     data = pd.read_csv('labeled.csv', index_col=0)
     data = downscale_digits(data, (50, 50), (10, 10))
-    print(data)
+    data.to_csv('downscaled_10_10.csv')
