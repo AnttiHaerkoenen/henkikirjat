8ce0e2f52898c0caa50989c632b7353d8001a0fd
antth <antth@hotmail.fi>
2019-10-01 17:35:24 +0300

    MLP model

--- src/ml/cnn.py
+++ src/ml/cnn.py
@@ -3,7 +3,10 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-# import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
 
 from src.ml.feature_extraction import downscale_digits
 
@@ -12,8 +15,25 @@ if __name__ == '__main__':
     split = 10000
     os.chdir('../../data')
     data = pd.read_csv('downscaled_10_10.csv')
-    label = data['label'].values
-    y_train, y_test = label[:split], label[split:]
-    x_train, x_test = np.vsplit(data.drop['label'].values, split)
-    print(y_test)
-    print(x_test)
+    data = data[pd.notna(data['label'])]
+
+    X = data.drop(columns=['label'])
+    y = data['label'].astype(int)
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        test_size=0.2,
+        shuffle=True,
+        random_state=42,
+    )
+    model = MLPClassifier(
+        hidden_layer_sizes=(100, 100),
+        max_iter=2000,
+    )
+    model.fit(X_train, y_train)
+    y_predict = model.predict(X_test)
+    errors = confusion_matrix(y_test, y_predict)
+    report = classification_report(y_test, y_predict)
+    print(errors)
+    print(report)
